2ad45347fccfaa8c1c2f0cc13e725757da7e62f5
Swagatika <reetu.swagatika@gmail.com>
2015-09-28 08:19:08 -0400

    Initial Commit

---
+++ RomanNumeral-SwiftTests/RomanNumeral_SwiftTests.swift
@@ -0,0 +1,36 @@
+//
+//  RomanNumeral_SwiftTests.swift
+//  RomanNumeral-SwiftTests
+//
+//  Created by Swagatika on 9/28/15.
+//  Copyright (c) 2015 swagatika. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class RomanNumeral_SwiftTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
